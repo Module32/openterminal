37d7f246e953570bbb6a8d4d4e23e3759fb752e0
Module64 <78182297+Module32@users.noreply.github.com>
2022-02-10 00:08:12 +0000

    testing dashboard

---
+++ openterminal/pages/dashboard.js
@@ -0,0 +1,19 @@
+import { useSession } from "next-auth/react"
+import Layout from '../components/layout'
+import Image from 'next/image'
+import Link from 'next/link'
+
+export default function SignIn() {
+  const { data: session, status } = useSession()
+  if (status !== "authenticated") { return 403 }
+  return (
+    <>
+      <Layout>
+      <div className="hometop">
+        <h2 style={{fontSize: '40px'}}>{session.user.name}</h2>
+        <p>{session.email}</p>
+        </div>
+      </Layout>
+    </>
+  )
+}
