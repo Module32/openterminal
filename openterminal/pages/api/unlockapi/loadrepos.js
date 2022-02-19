6b50e72083b0c79edc1a49265265b8b9f4d6b212
Module64 <78182297+Module32@users.noreply.github.com>
2022-02-19 15:06:34 -0500

    pls work plsworkwplsow

--- openterminal/pages/api/unlockapi/loadrepos.js
+++ openterminal/pages/api/unlockapi/loadrepos.js
@@ -4,8 +4,8 @@ export async function loadrepos(req, res) {
   const octokit = new Octokit({
     auth: process.env.GITHUB_AUTH_TOKEN
   })
-  const repos = await octokit.request('GET /repos/{owner}', {
-    owner: 'Module64',
+  const repos = await octokit.request('GET /users/{username}/repos', {
+    username: 'Module32',
   })
 
   return res.status(200).json({ repos: repos })
