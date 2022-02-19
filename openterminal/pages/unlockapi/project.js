3dbac3a4d8a564fbe303d6154e98b1e99f7e82c2
Module64 <78182297+Module32@users.noreply.github.com>
2022-02-19 12:18:58 -0500

    working on unlockapi...

--- openterminal/pages/unlockapi/project.js
+++ openterminal/pages/unlockapi/project.js
@@ -1,13 +1,14 @@
 import { Octokit } from '@octokit/rest';
 
-export default async function Project() {
-  const octokit = new Octokit({
-    auth: process.env.GITHUB_AUTH_TOKEN
-  })
-  const repos = await octokit.request('GET /repos/{owner}', {
-    owner: 'Module64',
-  })
-  return (
-    repos
-  )
+export default function Project() {
+  export default async (req, res) => {
+    const octokit = new Octokit({
+      auth: process.env.GITHUB_AUTH_TOKEN
+    })
+    const repos = await octokit.request('GET /repos/{owner}', {
+      owner: 'Module64',
+    })
+    console.log(repos);
+  }
+  return 'success'
 }
