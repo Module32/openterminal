fce0f5d0822093d28219a60bbf7ccd60e31bc8a6
Module64 <78182297+Module32@users.noreply.github.com>
2022-02-22 17:14:57 -0500

    trying to fix stuff

--- openterminal/pages/unlockapi/project.js
+++ openterminal/pages/unlockapi/project.js
@@ -1,9 +1,9 @@
 import useSWR from 'swr'
 import fetch from 'unfetch'
 
-const fetcher = url => fetch(url).then(r => r.json())
-
 export default function Project() {
+  const fetcher = url => fetch(url).then(r => r.json())
+  
   const { data, error } = useSWR('/api/unlockapi/loadrepos', fetcher)
 
   if (error) return <div>failed to load</div>
