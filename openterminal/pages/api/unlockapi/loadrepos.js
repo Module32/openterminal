5f92750a1af4f0101a703fb6a539734c9b02c41e
Module64 <78182297+Module32@users.noreply.github.com>
2022-02-22 17:45:54 -0500

    changes

--- openterminal/pages/api/unlockapi/loadrepos.js
+++ openterminal/pages/api/unlockapi/loadrepos.js
@@ -1,13 +1,10 @@
 const { Octokit } = require("octokit");
-import { useSession } from "next-auth/react"
 
 export default async function Loadrepos(req, res) {
-  const { data: session, status } = await useSession();
-  if (status !== "authenticated") { return 403 }
   const octokit = new Octokit({ auth: process.env.GITHUB_AUTH_TOKEN });
 
   let repos = await octokit.request('GET /users/{username}/repos', {
-    username: session.user.name
+    username: 'Module32'
   }) 
   let len = Object.keys(repos.data).length
   let repos_arr = []
