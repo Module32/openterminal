340fb4f70d86ae6a9ac0783e51366c8a774be670
Module64 <78182297+Module32@users.noreply.github.com>
2022-02-22 19:57:50 -0500

    updates to api

--- openterminal/pages/api/unlockapi/loadrepos/[user].js
+++ openterminal/pages/api/unlockapi/loadrepos/[user].js
@@ -1,10 +1,13 @@
 const { Octokit } = require("octokit");
+import { useRouter } from 'next/router';
 
 export default async function Loadrepos(req, res) {
   const octokit = new Octokit({ auth: process.env.GITHUB_AUTH_TOKEN });
+  const router = useRouter()
+  const { user } = router.query;
 
   let repos = await octokit.request('GET /users/{username}/repos', {
-    username: 'Module32'
+    username: user,
   }) 
   let len = Object.keys(repos.data).length
   let repos_arr = []
--- openterminal/pages/unlockapi/project.js
+++ openterminal/pages/unlockapi/project.js
@@ -1,10 +1,11 @@
 import useSWR from 'swr'
 import fetch from 'unfetch'
+import { useSession } from "next-auth/react"
 
 export default function Project() {
   const fetcher = url => fetch(url).then(r => r.json())
   
-  const { data, error } = useSWR('/api/unlockapi/loadrepos', fetcher)
+  const { data, error } = useSWR(`/api/unlockapi/loadrepos/${session.user.name}`, fetcher)
 
   if (error) return <div>failed to load</div>
   if (!data) return <div>loading...</div>
