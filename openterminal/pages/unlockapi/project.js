95cdd6ac37fb55ed08ce7387367c998151c817d9
Module64 <78182297+Module32@users.noreply.github.com>
2022-02-22 20:09:20 -0500

    attempting to fix

--- openterminal/pages/unlockapi/project.js
+++ openterminal/pages/unlockapi/project.js
@@ -6,8 +6,10 @@ export default function Project() {
   const fetcher = url => fetch(url).then(r => r.json())
   const { data: session, status } = useSession()
   if (status !== "authenticated") { return 403 }
+
+  let username = session.user.name;
   
-  const { ledata, error } = useSWR(`/api/unlockapi/loadrepos/${session.user.name}`, fetcher)
+  const { ledata, error } = useSWR(`/api/unlockapi/loadrepos/${username}`, fetcher)
 
   if (error) return <div>failed to load</div>
   if (!ledata) return <div>loading...</div>
