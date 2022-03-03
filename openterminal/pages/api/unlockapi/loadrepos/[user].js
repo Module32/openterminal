adbde51ae0a48416126d7209cf2017b8a2cb5b47
Module32 <78182297+Module32@users.noreply.github.com>
2022-03-03 18:12:33 -0500

    pls work lol

--- openterminal/pages/api/unlockapi/loadrepos/[user].js
+++ openterminal/pages/api/unlockapi/loadrepos/[user].js
@@ -1,19 +1,23 @@
 const { Octokit } = require("octokit");
 import { useRouter } from 'next/router';
 
-export default async function Loadrepos(req, res) {
-  const router = await useRouter()
-  const octokit = new Octokit({ auth: process.env.GITHUB_AUTH_TOKEN });
-  const { user } = router.query;
+export default async function handler(req, res) {
+  try {
+    const router = await useRouter()
+    const octokit = new Octokit({ auth: process.env.GITHUB_AUTH_TOKEN });
+    const { user } = router.query;
 
-  let repos = await octokit.request('GET /users/{username}/repos', {
-    username: user,
-  }) 
-  let len = Object.keys(repos.data).length
-  let repos_arr = []
-  for (var i = 0; i < len; i++) {
-    repos_arr.push(repos.data[i].name)
+    let repos = await octokit.request('GET /users/{username}/repos', {
+      username: user,
+    }) 
+    let len = Object.keys(repos.data).length
+    let repos_arr = []
+    for (var i = 0; i < len; i++) {
+      repos_arr.push(repos.data[i].name)
+    }
+
+    return res.status(200).json({ user: user, repos: repos_arr })
+  } catch (err) {
+    res.status(500).json({ error: 'failed to load data' })
   }
-
-  return res.status(200).json({ user: user, repos: repos_arr })
 }
