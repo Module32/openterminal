47415cfeeccf4ee4012cacc04b97a1127284c82f
Module32 <78182297+Module32@users.noreply.github.com>
2022-03-04 18:54:36 -0500

    WORKWORKWORK

--- openterminal/pages/unlockapi/newproject.js
+++ openterminal/pages/unlockapi/newproject.js
@@ -4,16 +4,16 @@ import Layout from '../../components/layout'
 
 export default function Project() {
   const fetcher = url => fetch(url).then(r => r.json())
-  const { ledata, error } = useSWR("/api/unlockapi/src/loadrepos", fetcher)
-  console.log(ledata);
+  const { data, error } = useSWR("/api/unlockapi/src/loadrepos", fetcher)
+  console.log(data);
 
   if (error) return <div>failed to load</div>
-  if (!ledata) return <div>loading...</div>
+  if (!data) return <div>loading...</div>
 
   return (
     <>
       <Layout>
-        {ledata}
+        {data}
       </Layout>
     </>
   )
