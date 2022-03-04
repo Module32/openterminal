4bd2d7cf69225fcbd34f3cd6fe1f1b55a0dc129b
Module32 <78182297+Module32@users.noreply.github.com>
2022-03-04 18:51:04 -0500

    attempting to fix

--- openterminal/pages/unlockapi/newproject.js
+++ openterminal/pages/unlockapi/newproject.js
@@ -1,13 +1,11 @@
 import useSWR from 'swr'
 import fetch from 'unfetch'
-import { useSession } from "next-auth/react"
 import Layout from '../../components/layout'
 
 export default function Project() {
   const fetcher = url => fetch(url).then(r => r.json())
-  const { data: session, status } = useSession()
-  const { ledata, error } = useSWR(status === "authenticated" ? `/api/unlockapi/loadrepos/${session.user.name}` : null, fetcher)
-  if (status !== "authenticated") { return 403 }
+  const { ledata, error } = useSWR("/api/unlockapi/src/loadrepos", fetcher)
+  console.log(ledata);
 
   if (error) return <div>failed to load</div>
   if (!ledata) return <div>loading...</div>
