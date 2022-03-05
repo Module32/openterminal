60e0f26f7e84ae6f25da6dfe5efe9904ec2ba51a
Module32 <78182297+Module32@users.noreply.github.com>
2022-03-05 18:11:00 -0500

    plswork

--- openterminal/pages/unlockapi/newproject.js
+++ openterminal/pages/unlockapi/newproject.js
@@ -25,8 +25,7 @@ export default function Project() {
   const [content, setContent] = useState(<h4>Please select a repository!</h4>);
   const [query, setQuery] = useState("");
 
-  let fetched_content_repo = Getrepo(content);
-  console.log(fetched_content_repo);
+  let repo_content = []
 
   if (error) return <div>failed to load</div>
   if (!data) return <div>loading...</div>
@@ -42,7 +41,7 @@ export default function Project() {
   }).map((repo) =>
     <div key={repo} style={{padding: '5px', borderRadius: '10px', border: 'none', margin: '5px', backgroundColor: 'rgb(235, 235, 235, 0.7)', display: 'flex', flexDirection: 'row'}}>
       <h3 style={{marginLeft: '7px', color: 'black'}}>{repo}</h3>
-      <h3 style={{marginLeft: 'auto', marginRight: '7px'}}><span><Link href=""><a onClick={() => setContent(<h2><span className="grey">Connect </span>{data.user}/{repo}</h2>)}>Connect <FontAwesomeIcon icon="arrow-right" /></a></Link></span></h3>
+      <h3 style={{marginLeft: 'auto', marginRight: '7px'}}><span><Link href=""><a onClick={() => setContent(<h2><span className="grey">Connect </span>{data.user}/{repo}<br /><span style={{fontSize: '13px'}}>{Getrepo(content).description}</span></h2>)}>Connect <FontAwesomeIcon icon="arrow-right" /></a></Link></span></h3>
     </div>
   );
 
