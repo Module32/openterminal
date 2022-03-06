d1debfb855e00f77e0ec25a783d4524d831e1645
Module32 <78182297+Module32@users.noreply.github.com>
2022-03-06 11:26:26 -0500

    aa

--- openterminal/pages/unlockapi/newproject.js
+++ openterminal/pages/unlockapi/newproject.js
@@ -26,8 +26,14 @@ export default function Project() {
     }
   }).map((repo, index) =>
     <div key={index} style={{padding: '5px', borderRadius: '10px', border: 'none', margin: '5px', backgroundColor: 'rgb(235, 235, 235, 0.7)', display: 'flex', flexDirection: 'row'}}>
-      <h3 style={{marginLeft: '7px', color: 'black'}}>{repo}</h3>
-      <h3 style={{marginLeft: 'auto', marginRight: '7px'}}><span><Link href=""><a onClick={() => setContent(<><h2><span className="grey">Connect </span>{data.user}/{repo.name}<br /></h2><p>Start a new project under <span className="grey">.../unlockapi/</span>{data.user}/{repo.name}.</p></>)}>Connect <FontAwesomeIcon icon="arrow-right" /></a></Link></span></h3>
+      <h3 style={{marginLeft: '7px', color: 'black'}}>{repo.name}</h3>
+      <h3 style={{marginLeft: 'auto', marginRight: '7px'}}><span><Link href=""><a onClick={() => setContent(
+        <>
+          <h2><span className="grey">Connect </span>{data.user}/{repo.name}<br /></h2>
+          <p>Start a new project under <span className="grey">.../unlockapi/</span>{data.user}/{repo.name}.</p>
+        </>)}>
+        Connect <FontAwesomeIcon icon="arrow-right" /></a></Link></span>
+      </h3>
     </div>
   );
 
