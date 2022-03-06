178b838da12d5b6007d5d63c8a398e18f011b4ff
Module32 <78182297+Module32@users.noreply.github.com>
2022-03-06 18:30:19 -0500

    api update

--- openterminal/pages/api/unlockapi/src/loadrepos.js
+++ openterminal/pages/api/unlockapi/src/loadrepos.js
@@ -5,21 +5,16 @@ export default async function Loadrepos(req, res) {
   const session = await getSession({ req });
   if (session === null) { return 403; }
   const user = session.user.name;
-  try {
-    const octokit = new Octokit({ auth: process.env.GITHUB_AUTH_TOKEN });
+  const octokit = new Octokit({ auth: process.env.GITHUB_AUTH_TOKEN });
 
-    let repos = await octokit.paginate('GET /users/{username}/repos', {
-      username: user,
-    })
-    let repo_arr = []
-    let repo_names = []
-    repos.map(repo => {
-      repo_arr.push(repo);
-    })
+  let repos = await octokit.paginate('GET /users/{username}/repos', {
+    username: user,
+  })
+  let repo_arr = []
+  let repo_names = []
+  repos.map(repo => {
+    repo_arr.push(repo);
+  })
 
-    res.status(200).json({ user: user, repos: repo_arr })
-  } catch (err) {
-    console.log(err);
-    res.status(500).json({ error: 'failed to load data' })
-  }
+  res.status(200).json({ user: user, repos: repo_arr })
 }
