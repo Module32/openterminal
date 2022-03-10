c7cdd9dbe7920dedf9b96530e0bbc6105125b81d
Module32 <78182297+Module32@users.noreply.github.com>
2022-03-10 18:56:03 -0500

    hmmm

--- openterminal/pages/unlockapi/index.js
+++ openterminal/pages/unlockapi/index.js
@@ -13,25 +13,25 @@ export default function Index() {
         <>
         <Unlockapi>
             <div className="waves2" style={{padding: '50px', paddingTop: '60px'}}>
-          <div className="infocard" style={{padding: '100px', width: '100%'}}>  
+          <div className="infocard" style={{padding: '100px'}}>  
           <div style={{flexDirection: 'column', paddingBottom: '50px'}}>
               <h1 style={{fontSize: '70px', margin: '0'}}>Ensure your APIs are reliable.</h1>
-  <p style={{paddingBottom: '12px'}}>UnlockAPI is the most advanced tool to monitor your services.</p>
-  <div style={{flexDirection: 'row'}}>
-  <Link href="/newproject">
-  <a className="padding" style={{width: '100%'}}>Get started</a>
-  </Link>
+              <p style={{paddingBottom: '12px'}}>UnlockAPI is the most advanced tool to monitor your services.</p>
+              <div style={{flexDirection: 'column'}}>
+              <Link href="/newproject">
+              <a className="padding" style={{width: '100%'}}>Get started</a>
+              </Link>
 
-  <Link href="">
-  <a className="padding" style={{width: '100%'}}>Learn more</a>
-  </Link>
-  
-  </div>
+              <Link href="">
+              <a className="padding" style={{width: '100%'}}>Learn more</a>
+              </Link>
+              
+              </div>
   </div>
             <Image
                     src="/pics/unlockapi/mascot1.png"
                     width='760px'
-                    height='650px'
+                    height='550px'
                   />
             </div>
   </div>
