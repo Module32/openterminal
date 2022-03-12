b03037c07f73da987c4bb2f2cec5092234ec0e2c
Module32 <78182297+Module32@users.noreply.github.com>
2022-03-12 12:43:18 -0500

    haha

--- openterminal/pages/whyot.js
+++ openterminal/pages/whyot.js
@@ -7,6 +7,7 @@ import TextLoop from "react-text-loop"
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faArrowRight, faArrowCircleRight, faHeart } from '@fortawesome/fontawesome-free-solid'
 import Fade from 'react-reveal/Fade';
+import Typical from 'react-typical';
 
 import topbanner from '../public/pics/whyot/topbanner.png';
 
@@ -14,12 +15,20 @@ export default function Index() {
     return (
         <>
           <Layout>
-            <div style={{paddingTop: '100px'}}>
-                <div className="whyottopbanner" style={{paddingTop: '100px', paddingBottom: '100px'}}>
-                    <p>Why OT?</p>
-                    <h1><span style={{padding: '5px', borderRadius: '5px', backgroundColor: 'rgb(0, 0, 0, 0.2)', textShadow: '0px 5px 25px rgb(0, 0, 0, 0.4)'}}>Because OT is made for everyone.</span></h1>
+              <FadeUp>
+                <div style={{paddingTop: '80px'}}>
+                    <div className="whyottopbanner" style={{paddingTop: '100px', paddingBottom: '100px'}}>
+                        <p>Why OT?</p>
+                        <h1><span style={{padding: '5px', borderRadius: '5px', backgroundColor: 'rgb(0, 0, 0, 0.2)', textShadow: '0px 5px 25px rgb(0, 0, 0, 0.4)'}}>Because OT
+                        <span style={{color: '#43AA8B'}}>
+                            <Typical
+                                steps={['is made for everyone', 'helps out devs', 'supports students', 'helps your mum', 'is always growing', 'is reliable', 'just works']}
+                                loop={Infinity}
+                                wrapper="p"
+                            /></span>.</span></h1>
+                    </div>
                 </div>
-            </div>
+              </FadeUp>
         </Layout>
                       <Footer>
                       </Footer>
