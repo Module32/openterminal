4f8212c7f6381d6923b45c5072e2acfed9dbe923
Module32 <78182297+Module32@users.noreply.github.com>
2022-03-12 16:33:48 -0500

    grr

--- openterminal/pages/unlockapi/newproject.js
+++ openterminal/pages/unlockapi/newproject.js
@@ -6,32 +6,24 @@ import Link from 'next/link'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faArrowRight, faStar, faCodeBranch, faLanguage } from '@fortawesome/fontawesome-free-solid'
 import { useState } from "react";
+import Fade from 'react-reveal/Fade';
 
 export default function Project() {
   const fetcher = url => fetch(url).then(r => r.json())
   const { data, error } = useSWR("/api/unlockapi/src/loadrepos", fetcher)
   const [content, setContent] = useState(<h4>Please select a repository!</h4>);
   const [query, setQuery] = useState("");
-  let fetch_status = 0
 
-  if (error) fetch_status = 500
-  if (!data) fetch_status = 503
+  if (error) return <div>failed to load</div>
+  if (!data) return <div>loading...</div>
 
   if (!data.repos) return <div>Looks like there are no repos under your account!</div>
 
   const listItems = data.repos.filter(repo => {
-    if (fetch_status != 0) {
-      if (fetch_status === 500) {
-        return "An error occured while fetching the repositories."
-      } else {
-        return "Fetching your repositories..."
-      }
-    } else {
-      if (query === '') {
-        return repo;
-      } else if (repo.name.toLowerCase().includes(query.toLowerCase())) {
-        return repo;
-      }
+    if (query === '') {
+      return repo;
+    } else if (repo.name.toLowerCase().includes(query.toLowerCase())) {
+      return repo;
     }
   }).map((repo, index) =>
     <div key={index} style={{padding: '5px', borderRadius: '10px', border: 'none', margin: '5px', backgroundColor: 'rgb(235, 235, 235, 0.7)', display: 'flex', flexDirection: 'row'}}>
@@ -57,22 +49,24 @@ export default function Project() {
   return ( 
     <>
       <Layout>
-        <div className="waves2">
-          <h1 style={{marginTop: '60px'}}>New Project</h1>
-          <p>Welcome! Let&apos;s get you started with a new UnlockAPI Project.</p>
-          <div className="acrylic" style={{display: 'flex'}}>
-            <div style={{flex: '1', paddingRight: '15px'}}>
-              <h2>Choose a repository to import...</h2>
-              <input placeholder="Search up repo" onChange={event => setQuery(event.target.value)} style={{width: '98%'}} />
-              <div style={{height: '250px', overflow: 'scroll', padding: '10px', borderRadius: '10px', backgroundColor: 'rgb(46, 46, 46, 0.45)'}}>
-                {listItems}
+        <Fade>
+          <div className="waves2">
+            <h1 style={{marginTop: '60px'}}>New Project</h1>
+            <p>Welcome! Let&apos;s get you started with a new UnlockAPI Project.</p>
+            <div className="acrylic" style={{display: 'flex'}}>
+              <div style={{flex: '1', paddingRight: '15px'}}>
+                <h2>Choose a repository to import...</h2>
+                <input placeholder="Search up repo" onChange={event => setQuery(event.target.value)} style={{width: '98%'}} />
+                <div style={{height: '250px', overflow: 'scroll', padding: '10px', borderRadius: '10px', backgroundColor: 'rgb(46, 46, 46, 0.45)'}}>
+                  {listItems}
+                </div>
+              </div>
+              <div style={{flex: '1', borderLeft: '2px solid rgb(255, 255, 255, 0.2)', paddingLeft: '15px'}}>
+                {content}
               </div>
             </div>
-            <div style={{flex: '1', borderLeft: '2px solid rgb(255, 255, 255, 0.2)', paddingLeft: '15px'}}>
-              {content}
-            </div>
           </div>
-        </div>
+        </Fade>
       </Layout>
       <Footer>
       </Footer>
