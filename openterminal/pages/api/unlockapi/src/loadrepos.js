3644f7649eac0c274c528f052821de1a6290f6e8
Module32 <78182297+Module32@users.noreply.github.com>
2022-03-12 17:27:27 -0500

    optimization... or breaking code

--- openterminal/pages/api/unlockapi/src/loadrepos.js
+++ openterminal/pages/api/unlockapi/src/loadrepos.js
@@ -3,18 +3,12 @@ import { getSession } from "next-auth/react"
 
 export default async function Loadrepos(req, res) {
   const session = await getSession({ req });
-  if (session === null) { return 403; }
   const user = session.user.name;
   const octokit = new Octokit({ auth: process.env.GITHUB_AUTH_TOKEN });
 
   let repos = await octokit.paginate('GET /users/{username}/repos', {
     username: user,
   })
-  let repo_arr = []
-  let repo_names = []
-  repos.map(repo => {
-    repo_arr.push(repo);
-  })
 
-  res.status(200).json({ user: user, repos: repo_arr })
+  res.status(200).json({ user: user, repos: repos })
 }
