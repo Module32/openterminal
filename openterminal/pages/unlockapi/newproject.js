98a49b9987b7c22e8ff98d10e535fda20ab3826d
Module32 <78182297+Module32@users.noreply.github.com>
2022-03-12 17:19:11 -0500

    hi

--- openterminal/pages/unlockapi/newproject.js
+++ openterminal/pages/unlockapi/newproject.js
@@ -15,8 +15,8 @@ export default function Project() {
   const [content, setContent] = useState(<h4>Please select a repository!</h4>);
   const [query, setQuery] = useState("");
 
-  if (error) return <div className={styles.pendingapi}><span style={{color: '#f51d1d'}}><FontAwesomeIcon icon="exclamation" /> We couldn&apos;t load your repositories.</span></div>
-  if (!data) return <div className={styles.pendingapi}>Loading repositories...</div>
+  if (error) return <div className={styles.pendingapi}><h1><span style={{color: '#f51d1d'}}><FontAwesomeIcon icon="exclamation" /> We couldn&apos;t load your repositories.</span></h1></div>
+  if (!data) return <div className={styles.pendingapi}><h1>Loading repositories...</h1></div>
 
   if (!data.repos) return <div>Looks like there are no repos under your account!</div>
 
