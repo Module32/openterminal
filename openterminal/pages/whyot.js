8f1b6c1793ac55048b6af15aa38379a14a4c1521
Module32 <78182297+Module32@users.noreply.github.com>
2022-03-13 16:30:07 -0400

    ea

--- openterminal/pages/whyot.js
+++ openterminal/pages/whyot.js
@@ -20,9 +20,9 @@ export default function Index() {
                     <div className="whyottopbanner" style={{paddingTop: '100px', paddingBottom: '100px'}}>
                         <p>Why OT?</p>
                         <h1>
-                        <span style={{textShadow: '0px 5px 25px rgb(0, 0, 0, 0.5)'}}>
+                        <span style={{textShadow: '0px 5px 25px rgb(0, 0, 0, 0.5)', fontWeight: '700'}}>
                             <Typical
-                                steps={['Because OT is made for everyone', 1000, 'Because OT helps out devs', 1000, 'Because OT supports students', 1000, 'Because OT helps your mum', 500, 'Because OT is always growing', 1000, 'Because OT is reliable', 1000, 'Because OT just works', 1000]}
+                                steps={['Because OT is made for everyone', 1000, 'Because OT helps out devs', 1000, 'Because OT supports students', 1000, 'Because OT helps your mum', 250, 'Because OT is always growing', 1000, 'Because OT is reliable', 1000, 'Because OT just works', 1000]}
                                 loop={Infinity}
                             /></span></h1>
                     </div>
