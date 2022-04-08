6ca25e81c838711add72e168c0de342232f2dce1
Module32 <78182297+Module32@users.noreply.github.com>
2022-04-08 19:54:13 -0400

    sus

--- openterminal/pages/genopi/dashboard.js
+++ openterminal/pages/genopi/dashboard.js
@@ -2,7 +2,7 @@ import Layout from '../../components/layout'
 import Footer from '../../components/footer'
 import Link from 'next/link'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faCopy, faNewspaper } from '@fortawesome/fontawesome-free-solid'
+import { faCopy, faNewspaper, faRocket } from '@fortawesome/fontawesome-free-solid'
 import Fade from 'react-reveal/Fade';
 import { useSession } from "next-auth/react"
 
@@ -13,14 +13,15 @@ export default function Project() {
       <>
         <Layout>
             <div className="hometop" style={{backgroundColor: '#efefefff', color: 'black', textAlign: 'left'}}>
-                <div style={{backgroundColor: 'white', marginTop: '10px', padding: '25px', borderRadius: '10px', display: 'flex', boxShadow: '0px 5px 10px rgb(0, 0, 0, 0.15)'}}>
+                <div style={{backgroundColor: 'white', marginTop: '20px', padding: '25px', borderRadius: '10px', display: 'flex', boxShadow: '0px 5px 10px rgb(0, 0, 0, 0.11)'}}>
                     <h1 style={{fontWeight: '700', flexDirection: 'row'}}><span className="grey">Welcome back,</span> {session.user.name}</h1>
-                    <h1 style={{flexDirection: 'row', marginLeft: 'auto'}}>0XP | Level 0</h1>
+                    <h1 style={{flexDirection: 'row', marginLeft: 'auto'}}><FontAwesomeIcon icon="rocket"></FontAwesomeIcon> 0XP | Level 0</h1>
                 </div>
                 <p>Create a...</p>
                 <div style={{display: 'flex'}}>
                     <Link href=""><a className="boxy" style={{flexDirection: 'row', color: 'black', backgroundImage: "url('/pics/genopi/newtest.png')", backgroundSize: 'cover'}}><span style={{backgroundColor: 'white', padding: '5px', borderRadius: '5px'}}>New Test</span></a></Link>
                     <Link href=""><a className="boxy" style={{flexDirection: 'row', color: 'black', backgroundImage: "url('/pics/genopi/newreading.png')", backgroundSize: 'cover'}}><span  style={{backgroundColor: 'white', padding: '5px', borderRadius: '5px'}}>New Reading</span></a></Link>
+                    <Link href=""><a className="boxy" style={{flexDirection: 'row', color: 'black', backgroundImage: "url('/pics/genopi/newnotes.png')", backgroundSize: 'cover'}}><span  style={{backgroundColor: 'white', padding: '5px', borderRadius: '5px'}}>New Notes</span></a></Link>
                 </div>
             </div>
         </Layout>
