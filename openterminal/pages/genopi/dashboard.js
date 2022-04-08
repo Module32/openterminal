f715afce396918b6db609bfa649d7f60d581a284
Module32 <78182297+Module32@users.noreply.github.com>
2022-04-08 17:33:26 -0400

    eee

--- openterminal/pages/genopi/dashboard.js
+++ openterminal/pages/genopi/dashboard.js
@@ -11,18 +11,19 @@ export default function Project() {
     if (status !== "authenticated") { return 403 }
     return (
       <>
-        <Layout style={{backgroundColor: '#efefefff', color: 'black'}}>
-            <div className="hometop">
-                <div style={{color: 'white', padding: '25px', borderRadius: '5px'}}>
+        <Layout>
+            <div className="hometop" style={{backgroundColor: '#efefefff', color: 'black', textAlign: 'left'}}>
+                <div style={{backgroundColor: 'white', padding: '25px', borderRadius: '5px'}}>
                     <h1 style={{fontWeight: '700'}}><span className="grey">Welcome back,</span> {session.user.name}</h1>
                 </div>
                 <p>Create a...</p>
                 <div style={{display: 'flex'}}>
-                    <Link href=""><a className="boxy" style={{flexDirection: 'row'}}><FontAwesomeIcon icon="copy" style={{padding: '10px', borderRadius: '5px', backgroundColor: 'rgb(25, 186, 255, 0.3)', color: 'rgb(25, 186, 255)'}}></FontAwesomeIcon> New Test</a></Link>
-                    <Link href=""><a className="boxy" style={{flexDirection: 'row'}}><FontAwesomeIcon icon="newspaper" style={{padding: '10px', borderRadius: '5px', backgroundColor: 'rgb(25, 186, 255, 0.3)', color: 'rgb(25, 186, 255)'}}></FontAwesomeIcon> New Reading</a></Link>
+                    <Link href=""><a className="boxy" style={{flexDirection: 'row'}}><FontAwesomeIcon icon="copy" style={{padding: '10px', borderRadius: '5px', backgroundColor: 'rgb(25, 186, 255, 0.3)', color: 'rgb(25, 186, 255)'}}>New Test</FontAwesomeIcon></a></Link>
+                    <Link href=""><a className="boxy" style={{flexDirection: 'row'}}><FontAwesomeIcon icon="newspaper" style={{padding: '10px', borderRadius: '5px', backgroundColor: 'rgb(25, 186, 255, 0.3)', color: 'rgb(25, 186, 255)'}}>New Reading</FontAwesomeIcon></a></Link>
                 </div>
             </div>
         </Layout>
+        <Footer></Footer>
       </>
     )
 }
