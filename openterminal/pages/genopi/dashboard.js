46500261073c008867106cddadb9cf774d12088e
Module32 <78182297+Module32@users.noreply.github.com>
2022-04-08 20:00:07 -0400

    aa

--- openterminal/pages/genopi/dashboard.js
+++ openterminal/pages/genopi/dashboard.js
@@ -13,7 +13,7 @@ export default function Project() {
       <>
         <Layout>
             <div className="hometop" style={{backgroundColor: '#efefefff', color: 'black', textAlign: 'left'}}>
-                <div style={{backgroundColor: 'white', marginTop: '20px', padding: '25px', borderRadius: '10px', display: 'flex', boxShadow: '0px 5px 10px rgb(0, 0, 0, 0.11)'}}>
+                <div style={{backgroundColor: 'white', marginTop: '20px', padding: '25px', borderRadius: '10px', display: 'flex', boxShadow: '0px 5px 15px rgb(0, 0, 0, 0.11)'}}>
                     <h1 style={{fontWeight: '700', flexDirection: 'row'}}><span className="grey">Welcome back,</span> {session.user.name}</h1>
                     <h1 style={{flexDirection: 'row', marginLeft: 'auto'}}><FontAwesomeIcon icon="rocket"></FontAwesomeIcon> 0XP | Level 0</h1>
                 </div>
