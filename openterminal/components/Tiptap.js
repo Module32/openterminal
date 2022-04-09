e8b1fe11168c93470ce8361ea49915406377a2a4
Module32 <78182297+Module32@users.noreply.github.com>
2022-04-09 12:18:12 -0400

    testing

--- openterminal/components/Tiptap.js
+++ openterminal/components/Tiptap.js
@@ -1,10 +1,22 @@
 import { useEditor, EditorContent } from '@tiptap/react'
 import StarterKit from '@tiptap/starter-kit'
+import Bold from "@tiptap/extension-bold";
+import Italic from "@tiptap/extension-italic";
+import Underline from "@tiptap/extension-underline";
+import Strike from "@tiptap/extension-strike";
+import Code from "@tiptap/extension-code";
+import Highlight from "@tiptap/extension-highlight";
 
 const Tiptap = () => {
   const editor = useEditor({
     extensions: [
       StarterKit,
+      Bold,
+      Italic,
+      Underline,
+      Strike,
+      Code,
+      Highlight,
     ],
     content: '###### Let&apos;s get writing!',
   })
