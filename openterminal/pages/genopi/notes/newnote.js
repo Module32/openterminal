097851b521a38b42eec288e3f2731b7134077a53
Module32 <78182297+Module32@users.noreply.github.com>
2022-04-09 09:51:40 -0400

    bru

--- openterminal/pages/genopi/notes/newnote.js
+++ openterminal/pages/genopi/notes/newnote.js
@@ -5,7 +5,7 @@ import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faCopy, faNewspaper, faRocket } from '@fortawesome/fontawesome-free-solid'
 import Fade from 'react-reveal/Fade';
 import { useSession } from "next-auth/react"
-// import MDEditor from "@uiw/react-md-editor";
+import MDEditor from "@uiw/react-md-editor";
 import { useState } from "react";
 
 const mkdStr = "###### Note time!"
