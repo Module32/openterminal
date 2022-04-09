f380f36e3774f7c271ba49764e3a5671ddf580b1
Module32 <78182297+Module32@users.noreply.github.com>
2022-04-08 20:17:37 -0400

    AAAAA

---
+++ openterminal/pages/genopi/notes/newnote.js
@@ -0,0 +1,30 @@
+import Layout from '../../components/layout'
+import Footer from '../../components/footer'
+import Link from 'next/link'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faCopy, faNewspaper, faRocket } from '@fortawesome/fontawesome-free-solid'
+import Fade from 'react-reveal/Fade';
+import { useSession } from "next-auth/react"
+import MDEditor from "@uiw/react-md-editor";
+import { useState } from "react";
+
+const mkdStr = "###### Let&apos;s take some notes!"
+
+export default function Project() {
+    const { data: session, status } = useSession()
+    const [markdown, setMarkdown] = useState(mkdStr);
+    if (status !== "authenticated") { return 403 }
+    return (
+      <>
+        <Layout>
+            <div className="hometop" style={{backgroundColor: '#efefefff', color: 'black', textAlign: 'left'}}>
+                <input placeholder="New Note"></input>
+                <div data-color-mode="light">
+                    <MDEditor height={200} value={markdown} onChange={setMarkdown} />
+                </div>
+            </div>
+        </Layout>
+        <Footer></Footer>
+      </>
+    )
+}
