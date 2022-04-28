38e4818a55269ec23dfa4ffbf900b009fae09db8
Module32 <derivativeco2021@gmail.com>
2022-04-28 18:02:24 -0400

    oh

--- openterminal/pages/genopi/tests/newtest.js
+++ openterminal/pages/genopi/tests/newtest.js
@@ -14,12 +14,12 @@ export default function Project() {
       <>
         <Layout>
             <div className="hometop" style={{textAlign: 'left', paddingTop: '70px'}}>
-                <h2><span style={{ color: '#5d33f5' }}><FontAwesomeIcon icon={faFileAlt} /></span>New Test</h2>
+                <h2><span style={{ color: '#5d33f5' }}><FontAwesomeIcon icon={faFileAlt} /></span> New Test</h2>
                 <h3><input placeholder="Test Name" style={{width: '100%'}}></input></h3>
                 {[...Array(4)].map((x, i) => 
-                    <div key={i} style={{ backgroundColor: '#0f0f0f', padding: '10px', borderRadius: '10px', margin: '5px', border: '1px solid rgb(255, 255, 255, 0.4)', display: 'flex' }}>
-                    <h2>Question<br /><input style={{ width: '50%' }} placeholder="Enter a question"></input></h2>
-                    <h2>Answer<br /><input style={{ width: '50%' }} placeholder="Enter the answer"></input></h2>
+                    <div key={i} style={{ backgroundColor: '#0f0f0f', padding: '10px', borderRadius: '10px', marginBottom: '8px', border: '1px solid rgb(255, 255, 255, 0.3)', display: 'flex' }}>
+                    <h2>Question<br /><input style={{ flex: '0.9' }} placeholder="Enter a question"></input></h2>
+                    <h2>Answer<br /><input style={{ flex: '0.9' }} placeholder="Enter the answer"></input></h2>
                     </div>
                 )}
                 <h4><button style={{ width: '100%' }}><FontAwesomeIcon icon={faPlus} /> Add question</button></h4>
