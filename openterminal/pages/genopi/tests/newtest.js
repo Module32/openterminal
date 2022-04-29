3c8afa6b41ef0ff436e118dad33e0f2b77149a11
Module32 <derivativeco2021@gmail.com>
2022-04-29 15:58:18 -0400

    ok

--- openterminal/pages/genopi/tests/newtest.js
+++ openterminal/pages/genopi/tests/newtest.js
@@ -7,9 +7,9 @@ import { faFileAlt, faPlus } from '@fortawesome/fontawesome-free-solid'
 import { useSession } from "next-auth/react"
 import redirect from 'nextjs-redirect'
 
-function makeQuestionDiv(key) {
+function MakeQuestionDiv(props) {
   return (
-    <div key={key} style={{ backgroundColor: '#0f0f0f', padding: '10px', borderRadius: '10px', marginBottom: '8px', border: '1px solid rgb(255, 255, 255, 0.3)' }}>
+    <div key={props.key} style={{ backgroundColor: '#0f0f0f', padding: '10px', borderRadius: '10px', marginBottom: '8px', border: '1px solid rgb(255, 255, 255, 0.3)' }}>
       <div style={{display: 'flex'}}>
         <h2>Question<br /><input style={{ flex: '1.5' }} placeholder="Enter a question"></input></h2>
         <h2>Answer<br /><input style={{ flex: '1.5' }} placeholder="Enter the answer"></input></h2>
@@ -30,9 +30,9 @@ export default function Project() {
                 <h2><span style={{ color: '#5d33f5' }}><FontAwesomeIcon icon={faFileAlt} /></span> New Test</h2>
                 <h3><input placeholder="Test Name" style={{width: '100%'}}></input></h3>
                 {[...Array(4)].map((x, i) => 
-                    { makeQuestionDiv(i) }
+                    <MakeQuestionDiv key={i} />
                 )}
-                <h4><button style={{ width: '100%' }} onClick={ makeQuestionDiv("no") }><FontAwesomeIcon icon={faPlus} /> Add question</button></h4>
+                <h4><button style={{ width: '100%' }} onClick={ <MakeQuestionDiv key="no" /> }><FontAwesomeIcon icon={faPlus} /> Add question</button></h4>
             </div>
         </Layout>
         <Footer></Footer>
