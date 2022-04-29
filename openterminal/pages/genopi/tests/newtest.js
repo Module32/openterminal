17f33ec3c0efdb98e4723d1603956e40fb270143
Module32 <derivativeco2021@gmail.com>
2022-04-29 15:50:43 -0400

    hm

--- openterminal/pages/genopi/tests/newtest.js
+++ openterminal/pages/genopi/tests/newtest.js
@@ -8,14 +8,16 @@ import { useSession } from "next-auth/react"
 import redirect from 'nextjs-redirect'
 
 function makeQuestionDiv(key) {
-  <div key={key} style={{ backgroundColor: '#0f0f0f', padding: '10px', borderRadius: '10px', marginBottom: '8px', border: '1px solid rgb(255, 255, 255, 0.3)' }}>
-    <div style={{display: 'flex'}}>
-      <h2>Question<br /><input style={{ flex: '1.5' }} placeholder="Enter a question"></input></h2>
-      <h2>Answer<br /><input style={{ flex: '1.5' }} placeholder="Enter the answer"></input></h2>
+  return (<>
+    <div key={key} style={{ backgroundColor: '#0f0f0f', padding: '10px', borderRadius: '10px', marginBottom: '8px', border: '1px solid rgb(255, 255, 255, 0.3)' }}>
+      <div style={{display: 'flex'}}>
+        <h2>Question<br /><input style={{ flex: '1.5' }} placeholder="Enter a question"></input></h2>
+        <h2>Answer<br /><input style={{ flex: '1.5' }} placeholder="Enter the answer"></input></h2>
+      </div>
+      <h4>Hint<span className="grey">(optional)</span><br />Provide a hint to answer the question: <input style={{ width: '100%' }} placeholder="Any hint?"></input></h4>
+      <h4>Explanation<span className="grey">(optional)</span><br />Explain the answer if a user gets it wrong: <input style={{ width: '100%' }} placeholder="Any explanation?"></input></h4>
     </div>
-    <h4>Hint<span className="grey">(optional)</span><br />Provide a hint to answer the question: <input style={{ width: '100%' }} placeholder="Any hint?"></input></h4>
-    <h4>Explanation<span className="grey">(optional)</span><br />Explain the answer if a user gets it wrong: <input style={{ width: '100%' }} placeholder="Any explanation?"></input></h4>
-  </div>
+  </>)
 }
 
 export default function Project() {
