2a8ee44f6b8f6c5065765dbe6944ff153bc7e2cc
Module32 <derivativeco2021@gmail.com>
2022-05-04 18:44:19 -0400

    hm

--- openterminal/pages/genopi/tests/newtest.js
+++ openterminal/pages/genopi/tests/newtest.js
@@ -18,6 +18,7 @@ export default function Project() {
       setQuestionList((prevQuestionList) =>
         prevQuestionList.filter((_, idx) => idx !== questionIndex)
       );
+      setQnum(questionList.length - 1);
     }    
 
     const MakeQuestion = (props) => {
@@ -25,7 +26,7 @@ export default function Project() {
         <div key={props.componentKey.toString()} style={{ backgroundColor: '#13141c', padding: '10px 20px', borderRadius: '10px', margin: '10px', border: '1px solid rgb(255, 255, 255, 0.3)' }}>
           <div style={{display: 'flex', margin: '0', padding: '0'}}>
             <h1><span className="grey">Question</span> {props.componentKey.toString()}</h1>
-            <button className="red" onClick={() => deleteQuestion(props.componentKey)} style={{marginLeft: "auto"}}><FontAwesomeIcon icon={faTrash} /></button>
+            <button className="red" onClick={() => deleteQuestion(props.componentKey)} style={{marginLeft: 'auto'}}><FontAwesomeIcon icon={faTrash} /></button>
           </div>
           <div style={{display: 'flex', margin: '0', padding: '0'}}>
             <h3 style={{margin: '0', padding: '0', flex: '0.7'}}>Question<br /><input placeholder="Enter a question" style={{width: '96%'}}></input></h3>
