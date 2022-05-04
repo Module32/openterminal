19cadb1de190565e29c0aa1d2732793ae8e999c8
Module32 <derivativeco2021@gmail.com>
2022-05-04 18:28:50 -0400

    aaaa

--- openterminal/pages/genopi/tests/newtest.js
+++ openterminal/pages/genopi/tests/newtest.js
@@ -15,14 +15,16 @@ export default function Project() {
 
     function deleteQuestion(questionIndex) {
       questionIndex -= 1;
-      setQnum(qnum - 1)
-      setQuestionList(questionList.splice(questionIndex, 1));
-    }
+      setQnum(qnum - 1);
+      setQuestionList((prevQuestionList) =>
+        prevQuestionList.filter((_, idx) => idx !== questionIndex)
+      );
+    }    
 
     const MakeQuestion = (props) => {
       return (
         <div key={props.componentKey.toString()} style={{ backgroundColor: '#13141c', padding: '10px 20px', borderRadius: '10px', margin: '10px', border: '1px solid rgb(255, 255, 255, 0.3)' }}>
-          <h1><span className="grey">Question</span> {props.componentKey.toString()} <span style={{marginLeft: "auto"}}><button className="red" onClick={deleteQuestion(props.componentKey)}><FontAwesomeIcon icon={faTrash} /></button></span></h1>
+          <h1><span className="grey">Question</span> {props.componentKey.toString()} <span style={{marginLeft: "auto"}}><button className="red" onClick={() => deleteQuestion(props.componentKey)}><FontAwesomeIcon icon={faTrash} /></button></span></h1>
           <div style={{display: 'flex', margin: '0', padding: '0'}}>
             <h3 style={{margin: '0', padding: '0', flex: '0.7'}}>Question<br /><input placeholder="Enter a question" style={{width: '96%'}}></input></h3>
             <h3 style={{margin: '0', padding: '0', flex: '0.7'}}>Answer<br /><input placeholder="Enter the answer" style={{width: '96%'}}></input></h3>
