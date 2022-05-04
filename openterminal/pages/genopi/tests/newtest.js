a491179af0a04a552b22fed6b99a419e083be4c1
Module32 <derivativeco2021@gmail.com>
2022-05-04 17:41:57 -0400

    lemme see

--- openterminal/pages/genopi/tests/newtest.js
+++ openterminal/pages/genopi/tests/newtest.js
@@ -3,7 +3,7 @@ import Footer from '../../../components/footer'
 import Tiptap from '../../../components/Tiptap'
 import Link from 'next/link'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faFileAlt, faPlus } from '@fortawesome/fontawesome-free-solid'
+import { faFileAlt, faPlus, faTrash } from '@fortawesome/fontawesome-free-solid'
 import { useSession } from "next-auth/react"
 import redirect from 'nextjs-redirect'
 import { useState } from "react"
@@ -13,10 +13,16 @@ export default function Project() {
     const [qnum, setQnum] = useState(1);
     const [questionList, setQuestionList] = useState([]);
 
+    const deleteQuestion = (questionIndex) => {
+      questionIndex -= 1;
+      setQnum(qnum - 1)
+      setQuestionList(questionList.splice(questionIndex, 1));
+    }
+
     const MakeQuestion = (props) => {
       return (
         <div key={props.componentKey.toString()} style={{ backgroundColor: '#13141c', padding: '10px 20px', borderRadius: '10px', margin: '10px', border: '1px solid rgb(255, 255, 255, 0.3)' }}>
-          <h1><span className="grey">Question</span> {props.componentKey.toString()}</h1>
+          <h1><span className="grey">Question</span> {props.componentKey.toString()} <span style={{marginLeft: "auto"}}><button className="red" onClick={deleteQuestion(props.componentKey)}><FontAwesomeIcon icon={faTrash} /></button></span></h1>
           <div style={{display: 'flex', margin: '0', padding: '0'}}>
             <h3 style={{margin: '0', padding: '0', flex: '0.7'}}>Question<br /><input placeholder="Enter a question" style={{width: '96%'}}></input></h3>
             <h3 style={{margin: '0', padding: '0', flex: '0.7'}}>Answer<br /><input placeholder="Enter the answer" style={{width: '96%'}}></input></h3>
