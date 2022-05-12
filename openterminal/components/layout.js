caee191fd4efe5d6591a12318018f80ba0de0731
Module32 <derivativeco2021@gmail.com>
2022-05-12 19:11:20 -0400

    💀

--- openterminal/components/layout.js
+++ openterminal/components/layout.js
@@ -50,9 +50,9 @@ export default function Layout({ children, home }) {
               <a className="navbar">Team</a>
             </Link></li>
 
-            <li className="navbar" style={{justifyContent: 'flex-end'}}>
+            <li className="navbar" style={{marginLeft: 'auto'}}>
               { session ? 
-                <span style={{padding: '0', margin: '0'}}><Link href=""><a className="navbar" style={{paddingLeft: '7px'}}><FontAwesomeIcon icon={faBell} style={{color: 'rgb(146)'}} /></a></Link> <Link href=""><a className="navbar" style={{paddingLeft: '7px'}}><FontAwesomeIcon icon={faBookmark} style={{color: 'rgb(146)'}} /></a></Link> {session.user.name} <button className="navbar" onClick={() => signOut()}><FontAwesomeIcon icon={faArrowRight} style={{color: 'rgb(146)'}} /></button></span> : 
+                <span style={{padding: '0', margin: '0'}}><Link href=""><a className="navbar" style={{paddingRight: '7px'}}><FontAwesomeIcon icon={faBell} style={{color: 'rgb(146, 146, 146)'}} /></a></Link> <Link href=""><a className="navbar" style={{paddingRight: '7px'}}><FontAwesomeIcon icon={faBookmark} style={{color: 'rgb(146, 146, 146)'}} /></a></Link> {session.user.name} <button className="navbar" onClick={() => signOut()}><FontAwesomeIcon icon={faArrowRight} style={{color: 'rgb(146, 146, 146)'}} /></button></span> : 
                 <span style={{padding: '0', margin: '0'}}><Link href="/login"><a className="navbar">Login</a></Link> <span className="grey">|</span> <Link href="/signup"><a className="navbar">Sign up</a></Link></span>
               }
             </li>
