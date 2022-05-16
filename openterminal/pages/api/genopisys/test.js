778b187d7d1932e1cbdcac79cef15bf013f7c750
Module32 <derivativeco2021@gmail.com>
2022-05-16 16:17:36 -0400

    aaa

--- openterminal/pages/api/genopisys/test.js
+++ openterminal/pages/api/genopisys/test.js
@@ -1,15 +1,14 @@
 import db from "../../../util/db.js"
 
 export default async function handler(req, res) {
-    switch (req.method) {
-        case "POST":
+    if (req.method === 'POST') {
             let bodyObject = JSON.parse(req.body);
             let newTest = await db.collection("tests").insertOne(bodyObject);
             res.json(newTest.ops[0]);
             break;
-        case "GET":
+    } else if (req.method === 'GET') {
             const tests = await db.collection("tests").find({}).toArray();
             res.json({ status: 200, data: tests });
             break;
-  }
+    }
 }
