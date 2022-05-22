e1f9dfee2343cac10e1336323698d427538afc3c
Module32 <derivativeco2021@gmail.com>
2022-05-22 12:13:47 -0400

    planetscale go brrr

--- openterminal/pages/api/genopi/test.js
+++ openterminal/pages/api/genopi/test.js
@@ -1,13 +1,49 @@
-import {
-        createTest,
-        deleteTest,
-        getAllTests,
-        getTest,
-        updateTest
-} from '../../../prisma/test'
+import { prisma } from '../../../prisma/prisma'
       
-export default async function handle (req, res) {
-        try {
+export default async function handler(req, res) {
+  if (req.method === "GET") {
+    if (req.query.parameter) {
+      return await prisma.test.findUnique({
+        where: { parameter }
+      })
+    } else {
+      return await prisma.test.findMany({});
+    }
+  } else if (req.metod === "POST") {
+    const { name, creator, questions, date } = req.body;
+    return await prisma.test.create({
+      data: {
+        name,
+        creator,
+        questions,
+        date
+      }
+    })
+  } else if (req.method === "PUT") {
+    const { parameter, ...updateData } = req.body
+    return await prisma.test.update({
+      where: {
+        parameter
+      },
+      data: {
+        ...updateData
+      }
+    })
+  } else if (req.method === "DELETE") {
+    const { parameter } = req.body
+    return await prisma.test.delete({
+      where: {
+          parameter
+      }
+    })  
+  } else {
+    return res.status(405).json({ message: 'Method not allowed' });
+  }
+}
+
+/*
+
+try {
           switch (req.method) {
             case 'GET': {
               if (req.query.parameter) {
@@ -39,4 +75,5 @@ export default async function handle (req, res) {
         } catch (error) {
           return res.status(500).json({ ...error, message: error.message })
         }
-}
+
+*/
--- openterminal/pages/genopi/note/newnote.js
+++ openterminal/pages/genopi/note/newnote.js
@@ -16,9 +16,7 @@ export default function Project() {
             <div className="hometop" style={{textAlign: 'left', paddingTop: '70px'}}>
                 <h1><span style={{ color: '#f7a427ff' }}><FontAwesomeIcon icon={faStickyNote} /></span> New Note Session</h1>
                 <h3><input placeholder="Title goes here...!" style={{width: '100%'}}></input></h3>
-                <Tiptap content="Let&apos;s write something!" readonly={false}/>
-                <Tiptap content="this is read-only" readonly={true}/>
-                <Tiptap content="one more lil test"/>
+                <Tiptap content="Let&apos;s write something!"/>
             </div>
         </Layout>
         <Footer></Footer>
