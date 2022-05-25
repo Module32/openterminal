898ec2aeab7dc5ce5b3667a47988939aabcde607
Module32 <derivativeco2021@gmail.com>
2022-05-24 20:23:19 -0400

    a

--- openterminal/pages/genopi/test/[id]/play.js
+++ openterminal/pages/genopi/test/[id]/play.js
@@ -3,9 +3,10 @@ import Layout from '../../../../components/layout'
 import Footer from '../../../../components/footer'
 import Link from 'next/link'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faArrowLeft, faArrowUpRightDots, faLightbulb, faCheck, faFire } from '@fortawesome/free-solid-svg-icons'
+import { faArrowLeft, faArrowUpRightDots, faLightbulb, faCheck, faFire, faFlagCheckered, faClock } from '@fortawesome/free-solid-svg-icons'
 import { useSession } from "next-auth/react"
 import { useState, useEffect } from "react"
+import Fade from 'react-reveal/Fade';
 
 export default function Play() {
     const router = useRouter()
@@ -16,6 +17,10 @@ export default function Play() {
     const [explanation, setExplanation] = useState("");
     const [disabled, setDisabled] = useState(false);
     const [score, setScore] = useState(0);
+    const [highStreak, setHighStreak] = useState(0);
+    const [correct, setCorrect] = useState(0);
+    const [avgTime, setAvgTime] = useState(0);
+    const [screen, setScreen] = useState("test");
     const { pid } = router.query
 
     let test = {
@@ -39,11 +44,13 @@ export default function Play() {
     let questions = test["questions"];
 
     const handleAnswerSumbit = (index, answer, time) => {
+        setAvgTime(avgTime + time)
         if (index + 1 <= questions.length) {
             let question = questions[index]
             if (question[`answer${index+1}`] === answer) {
                 setButtonStyle("green")
                 setDisabled(true);
+                setCorrect(correct + 1);
                 let secs = Math.floor(time / 1000);
                 let pointsDeducted = Math.floor(1.7**secs);
                 if (pointsDeducted > 80) pointsDeducted = 80;
@@ -56,8 +63,13 @@ export default function Play() {
                         setButtonStyle("neutral");
                         setDisabled(false);
                     }, 1500)
+                } else {
+                    setTimeout(() => {
+                        setScreen("finish")
+                    }, 1500)
                 }
             } else {
+                if (streak > highStreak) setHighStreak(streak);
                 setStreak(0)
                 setButtonStyle("red")
                 setDisabled(true);
@@ -72,8 +84,6 @@ export default function Play() {
                     }}>Got it</button>
                 </>)
             }
-        } else {
-            console.log("this is the end")
         }
     }
     
@@ -98,6 +108,7 @@ export default function Play() {
             }
         }
         let time = Date.now();
+        answers = answers.sort(() => (Math.random() > 0.5) ? 1 : -1)
 
         return (<>
             <div>
@@ -115,32 +126,45 @@ export default function Play() {
                     <Link href="/genopi/dashboard" ><a className="padding neutral"><FontAwesomeIcon icon={faArrowLeft} /></a></Link>
                     <p className="grey" style={{margin: 'auto'}}>{test.name}</p>
                 </div>
-                <div style={{display: 'flex'}}>
-                    <div style={{flex: '1', padding: '10px 20px'}}>
-                        <div style={{display: 'flex'}}>
-                            <p>{questionNumber + 1} <span className="grey">of {questions.length}</span></p>
-                            <p style={{marginLeft: 'auto'}}><span style={{color: '#ff3624'}}><FontAwesomeIcon icon={faArrowUpRightDots} /></span> {score} XP</p>
+                { screen === "test" ? <>
+                    <div style={{display: 'flex'}}>
+                        <div style={{flex: '1', padding: '10px 20px'}}>
+                            <div style={{display: 'flex'}}>
+                                <p>{questionNumber + 1} <span className="grey">of {questions.length}</span></p>
+                                <p style={{marginLeft: 'auto'}}><span style={{color: '#ff3624'}}><FontAwesomeIcon icon={faArrowUpRightDots} /></span> {score} XP</p>
+                            </div>
+                            <h1 style={{fontSize: '2.4em'}}>{questions[questionNumber][`question${questionNumber + 1}`]}</h1>
+                            {questions[questionNumber][`hint${questionNumber + 1}`] !== "" ? (<><p><span style={{color: '#ffa70f'}}><FontAwesomeIcon icon={faLightbulb} /></span> {questions[questionNumber][`hint${questionNumber + 1}`]}</p></>) : null}
+                            {explanation !== "" ? explanation : null}
                         </div>
-                        <h1 style={{fontSize: '2.4em'}}>{questions[questionNumber][`question${questionNumber + 1}`]}</h1>
-                        {questions[questionNumber][`hint${questionNumber + 1}`] !== "" ? (<><p><span style={{color: '#ffa70f'}}><FontAwesomeIcon icon={faLightbulb} /></span> {questions[questionNumber][`hint${questionNumber + 1}`]}</p></>) : null}
-                        {explanation !== "" ? explanation : null}
+                        <div style={{flex: '1', paddingTop: '25px', borderLeft: '2px solid rgb(255, 255, 255, 0.2)'}}>
+                            <div style={{padding: '10px'}}>
+                                {CreateAnswers(questionNumber)}
+                            </div>
+                            <div style={{width: '100%', padding: '10px', backgroundColor: streak > 9 ? '#eb4034' : streak > 4 ? '#fc7826' : '' }}>
+                                <p style={{ color: streak > 9 ? 'white' : streak > 4 ? '#242323' : 'rgb(255, 255, 255, 0.4)' }}>
+                                    <FontAwesomeIcon icon={faFire} /> { streak > 9 ?
+                                        <span><strong>You&apos;re on fire!</strong> Streak of <strong>{streak}</strong></span> :
+                                        streak > 4 ?
+                                        <span><strong>Nice!</strong> Streak of <strong>{streak}</strong></span> :
+                                        <span>Streak of <strong>{streak}</strong></span>
+                                    }
+                                </p>
+                            </div>
+                        </div>
                     </div>
-                    <div style={{flex: '1', paddingTop: '25px', borderLeft: '2px solid rgb(255, 255, 255, 0.2)'}}>
-                        <div style={{padding: '10px'}}>
-                            {CreateAnswers(questionNumber)}
+                </> : <>
+                    <Fade in>
+                        <div style={{padding: '10px', paddingTop: '30px', textAlign: 'center'}}>
+                            <h1><span style={{color: '#fac30f'}}><FontAwesomeIcon icon={faFlagCheckered} /></span> Congrats, you finished!</h1>
+                            <h3>
+                                <span style={{color: '#1ac74e'}}><FontAwesomeIcon icon={faCheck} /></span> {correct} correct out of {questions.length}<br />
+                                <span style={{color: '#242323'}}><FontAwesomeIcon icon={faFire} /></span> High streak of {highStreak}<br />
+                                <FontAwesomeIcon icon={faClock} /> Average time of {Math.floor((avgTime / questions.length) / 1000)} secs
+                            </h3>
                         </div>
-                        <div style={{width: '100%', padding: '10px', backgroundColor: streak > 9 ? '#eb4034' : streak > 4 ? '#fc7826' : '' }}>
-                            <p style={{ color: streak > 9 ? 'white' : streak > 4 ? '#242323' : 'rgb(255, 255, 255, 0.4)' }}>
-                                <FontAwesomeIcon icon={faFire} /> { streak > 9 ?
-                                    <span><strong>You&apos;re on fire!</strong> Streak of <strong>{streak}</strong></span> :
-                                    streak > 4 ?
-                                    <span><strong>Nice!</strong> Streak of <strong>{streak}</strong></span> :
-                                    <span>Streak of <strong>{streak}</strong></span>
-                                }
-                            </p>
-                        </div>
-                    </div>
-                </div>
+                    </Fade>
+                </>}
             </Layout>
             <Footer>
 
