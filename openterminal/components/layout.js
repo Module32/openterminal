50cf46f32813102f316f94eb901ecd68b8a02cae
Module32 <derivativeco2021@gmail.com>
2022-06-01 17:01:04 -0400

    aah

--- openterminal/components/layout.js
+++ openterminal/components/layout.js
@@ -1,28 +1,21 @@
 import Head from 'next/head'
-import styles from './layout.module.css'
 import Link from 'next/link'
 import Image from 'next/image'
 import React, { useState, useEffect } from "react"
-import Favicon from 'react-favicon';
 import { useSession, signIn, signOut } from "next-auth/react"
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faBell, faBookmark, faArrowRight } from '@fortawesome/free-solid-svg-icons'
 
-const name = 'Module64'
-export const siteTitle = 'Open Terminal'
-
 export default function Layout({ children, home }) {
   const { data: session } = useSession();
   return (
-    <div className={styles.container}>
+    <div className="">
       <Head>
         <meta property="og:description" content="Open Terminal is focused on delivering software and hardware solutions for all people. Find tools that help you create, teach, and do things better." />
-        <meta name="og:title" content={siteTitle} />
+        <meta name="og:title" content="Open Terminal" />
         <meta name="twitter:card" content="summary_large_image" />
         <title>Open Terminal - Creating for all</title>
       </Head>
-      <Favicon url="../public/pics/logo.png"/>
-      <header className={styles.header}>
           <>
           <ul className="navbar">
           <li className="logo">
@@ -58,10 +51,8 @@ export default function Layout({ children, home }) {
             </li>
           </ul>
           </>
-      </header>
       <hr></hr>
-      <div className={styles.card} style={{marginTop: '-20px'}}><main>{children}</main></div>
+      <div><main>{children}</main></div>
     </div>
-
   )
 }
