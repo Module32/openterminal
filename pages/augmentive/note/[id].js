b10c4a0de55f6bf69cea03428113f29da06a6689
Module32 <derivativeco2021@gmail.com>
2022-07-02 10:02:36 -0400

    Recoded db

--- pages/augmentive/note/[id].js
+++ pages/augmentive/note/[id].js
@@ -9,22 +9,20 @@ import { connectToDatabase } from "../../../util/db";
 import Custom404 from '../../404.js'
 import { ObjectId } from 'mongodb'
 
-export default function Project({ genouser, dbnote }) {
+export default function Project({ dbnote }) {
     const [ title, setTitle ] = useState(dbnote.title || 'Untitled');
-    const [ star, setStar ] = useState(false);
+    const [ star, setStar ] = useState(dbnote.starred || false);
     const [ shareModal, setShareModal ] = useState(false);
     const [ editability, setEditability ] = useState('edit');
     const [ viewability, setViewability ] = useState('public');
 
-    const { data: session, status } = useSession({
+    const { data: session } = useSession({
       required: true
     })
     
     if (!session) { return <Redirect text='OT login' link='/login'></Redirect> }
 
-    if (dbnote === 'Note does not exist') return <Custom404 />
-
-    if (title.length === 0) setTitle('Untitled')
+    if (dbnote === null) return <Custom404 />
 
     let token = process.env.API_TOKEN
 
@@ -85,28 +83,28 @@ export default function Project({ genouser, dbnote }) {
           
 
           {shareModal === true && <div className='fixed flex h-screen top-0 m-0 rounded-none hover:bg-black/30 w-screen bg-black/30 items-center justify-center cursor-default'>
-              <div className='bg-slate-300 font-medium w-4/12 rounded border border-slate-400'>
+              <div className='bg-slate-100 font-medium w-4/12 rounded border border-slate-400'>
                 <div className='p-5 text-left'>
                   <h1 className='text-3xl font-semibold flex'>Share {title} <span className='ml-auto mt-[-17px]'><button className='m-0 p-0 text-base bg-transparent border-none hover:bg-transparent text-slate-400 hover:text-slate-500' onClick={() => setShareModal(false)}><FontAwesomeIcon icon={faXmark} /></button></span></h1>
                 </div>
                 <div className='p-5 bg-slate-100'>
-                  <p className='text-gray'>Options</p>
-                  <p className='text-xl flex items-center'><span className='mr-2'>Make this note</span> {viewabilityArray.map(option =>
+                  <p className='text-xl'>Invite users</p>
+                  {viewability === 'private' && <p className='text-gray text-base leading-tight'>These users will be able to see your private note.</p>}
+                  <input className='w-full p-2 py-1 ml-0 bg-slate-300/30' type='email' placeholder='Search by email, press Enter to add'></input>
+                  <p className='text-gray'>Default viewability</p>
+                  <p className='text-xl flex items-center'>{viewabilityArray.map(option =>
                   <button
                     key={option}
                     className={`flex-1 ml-0 py-1 border-none rounded ${viewability === option.toLowerCase() ? 'bg-amber-500/75 hover:bg-amber-500/60' : 'bg-slate-400 hover:bg-slate-400/75'}`}
                     onClick={() => setViewability(option.toLowerCase())}>{option}</button>
                   )}</p>
-                  <p className={`text-xl flex items-center ${viewability === 'private' && 'text-gray-light'}`}><span className='mr-2'>Allow viewers to</span> {editabilityArray.map(mode =>
+                  <p className='text-gray'>Default editability</p>
+                  <p className={`text-xl flex items-center ${viewability === 'private' && 'text-gray-light'}`}>{editabilityArray.map(mode =>
                   <button
                     key={mode}
                     className={`flex-1 ml-0 py-1 border-none rounded ${editability === mode.toLowerCase() ? viewability === 'public' ? 'bg-amber-500/75 hover:bg-amber-500/60' : 'bg-amber-500/50 hover:bg-amber-500/50' : 'bg-slate-400 hover:bg-slate-400/75 disabled:bg-slate-300'}`}
                     onClick={() => setEditability(mode.toLowerCase())} disabled={viewability === 'private' ? true : false}>{mode}</button>
                   )}</p>
-                  <p className='text-gray mt-1'>Advanced</p>
-                  <p className='text-xl'>Invite users</p>
-                  {viewability === 'private' && <p className='text-gray text-base leading-tight'>These users will be able to see your private note.</p>}
-                  <input className='w-full p-2 py-1 ml-0 bg-slate-300/30' type='email' placeholder='Search by email, press Enter to add'></input>
                 </div>
                 <div className='px-0 bg-slate-300 text-center rounded-b'>
                   <button className={`flex border-none w-full overflow-scroll ml-0 rounded-none bg-transparent group active:text-green text-black hover:bg-transparent hover:text-gray`} onClick={() => navigator.clipboard.writeText(`openterminal.vercel.app/augmentive/note/${dbnote._id}`)}><FontAwesomeIcon icon={faClipboard} className='text-gray mt-[1px] mr-1 group-active:text-green' /> openterminal.vercel.app/augmentive/note/{dbnote._id}</button>
@@ -120,68 +118,49 @@ export default function Project({ genouser, dbnote }) {
 export async function getServerSideProps(context) {
   const { client } = await connectToDatabase();
   const session = await getSession(context);
-  let genouser;
   let dbnote;
+
   if (!session) return {
-      props: {
-        genouser: null,
-        dbnote: null
-      },
+    props: {
+      dbnote: null
+    },
   };
 
-  const routeid = context.params.id;
-
-  const db = client.db('Genopi');
-  await db.collection('users').findOne({ email: session['user']['email'] }).then(function(user) {
-      if (!user || user === null) {
-        const newUser = collection.insertOne({
-          email: session['user']['email'],
-          xp: 0,
-          level: 0,
-          owned: [],
-        });
-        genouser = newUser[0]
-      } else genouser = user
-  })
-
-  if (ObjectId.isValid(routeid) === false && routeid !== 'new') {
-    return {
-      props: {
-        genouser: JSON.parse(JSON.stringify(genouser)),
-        dbnote: 'Note does not exist'
-      }
-    }
-  } else if (ObjectId.isValid(routeid) === false && routeid === 'new') {
-    await db.collection('notes').findOne({ title: 'Untitled', content: 'Write something!', owner: session.user.email }).then(async function(note) {
-      if (!note || note === null) {
-        const newNote = await db.collection('notes').insertOne({
-          title: 'Untitled',
-          owner: session.user.email,
-          editability: 'edit',
-          viewability: 'private',
-          bgcolor: 'bg-white',
-          starred: false,
-          invUsers: [],
-          content: 'Write something!'
-        });
-        await db.collection('users').updateOne({ email: session.user.email }, { $addToSet: { 'owned': { id: newNote[0]._id } } }, function(err, res) {
-          if (err) throw err;
-        })
-        dbnote = newNote[0];
-      } else dbnote = note;
-    });
-  } else if (ObjectId.isValid(routeid) === true && routeid !== 'new') {
-    await db.collection('notes').findOne({ _id: ObjectId(routeid) }).then(async function(note) {
-      if (!note || note === null) {
-        return dbnote = 'Note does not exist'
-      } else dbnote = note;
-    })
-  };
+  let routeid;
+  if (context.params.id === 'new') { routeid = 'new' } else { routeid = context.params.id }
   
+  const db = client.db('Genopi');
+
+  if (routeid === 'new') {
+    fetchExistingNote = (query) => {
+      await db.collection('notes').findOne(query)
+      .then(note => note)
+    }
+    existingNote = fetchExistingNote({ title: 'Untitled', content: 'Write something!', owner: session.user.email })
+    if (existingNote !== null) return dbnote = existingNote
+    const newNote = {
+      title: 'Untitled',
+      owner: session.user.email,
+      editability: 'view',
+      viewability: 'private',
+      bgcolor: 'bg-white',
+      starred: false,
+      invUsers: [],
+      content: 'Write something!'
+    }
+    await db.collection('notes').insertOne(newNote).catch(err => { throw err });
+    dbnote = newNote;
+  } else {
+    if (ObjectId.isValid(routeid) === false) return dbnote = null;
+    await db.collection('notes').findOne({ _id: ObjectId(routeid) }).then(note => {
+      if (!note || note === null) { return dbnote = null } else { dbnote = note }
+    })
+  }
+
   return {
-      props: {
-        genouser: JSON.parse(JSON.stringify(genouser)),
-        dbnote: JSON.parse(JSON.stringify(dbnote))
-      },
-  };
+    props: {
+      dbnote: JSON.parse(JSON.stringify(dbnote))
+    }
+  }
+
 }
